241fcc96f65a26a3c9b9b1d6bf7c6f476cd34a1a
Van Hoan HOANG <v.hoang@certeurope.fr>
2022-04-01 00:00:25 +0200

    First

---
+++ src/main/java/com/certeurope/phone/TestSshApplication.java
@@ -0,0 +1,13 @@
+package com.certeurope.phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certeurope/phone/TestSshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certeurope.phone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
